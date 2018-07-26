f0e43c8a877b5f91a0b1c5eb0a25ed5dc9676f55
David Dvinskykh <david_dvinskykh@epam.com>
2018-07-26 17:50:01 +0300

    Added sorting to WordCounter.java

--- src/main/java/com/david/dvinskykh/graph/WordCounter.java
+++ src/main/java/com/david/dvinskykh/graph/WordCounter.java
@@ -1,29 +1,35 @@
 package com.david.dvinskykh.graph;
 
-import com.sun.javafx.binding.StringFormatter;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
 
 public class WordCounter {
 
     public static void getDuplicates(String inputPath, String outputPath) throws IOException {
-        Map<String, Long> frequencyMap = Files.lines(Paths.get(inputPath))
+        List<Map.Entry<String, Long>> frequencyMap = Files.lines(Paths.get(inputPath))
                 .map(StringUtils::split)
                 .flatMap(Arrays::stream)
-                .collect(Collectors.groupingByConcurrent(o -> o, Collectors.counting()));
-        List<CharSequence> results = frequencyMap
+                .collect(Collectors.groupingByConcurrent(o -> o, Collectors.counting()))
                 .entrySet().stream()
+                .sorted(Comparator.comparing(WordCounter::negateMapValue).thenComparing(Map.Entry::getKey))
+                .collect(Collectors.toList());
+        List<CharSequence> results = frequencyMap.stream()
                 .map(o -> (CharSequence) String.format("%d %s", o.getValue(), o.getKey()))
                 .collect(Collectors.toList());
         Files.write(Paths.get(outputPath), results);
+    }
+
+    private static Long negateMapValue(Map.Entry<String, Long> stringLongPair) {
+        return Math.negateExact(stringLongPair.getValue());
     }
 
 }
--- src/test/java/com/david/dvinskykh/graph/WordCounterTest.java
+++ src/test/java/com/david/dvinskykh/graph/WordCounterTest.java
@@ -1,5 +1,6 @@
 package com.david.dvinskykh.graph;
 
+import org.hamcrest.Matchers;
 import org.hamcrest.core.Is;
 import org.junit.Test;
 
@@ -7,26 +8,29 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class WordCounterTest {
 
     @Test
     public void getFrequentWords() throws IOException {
-        List<String> words = Arrays.asList("First Second", "Third First");
+        List<String> words = Arrays.asList("First Second Second Second", "Third First");
         File inputFile = File.createTempFile("input", "temp");
         File outputFile = File.createTempFile("input", "temp");
         Files.write(inputFile.toPath(), words);
         WordCounter.getDuplicates(inputFile.getPath(), outputFile.getPath());
         Map<String, Long> frequentWords = Files.lines(outputFile.toPath())
-                .collect(Collectors.toMap(k -> k.split(" ")[1], v -> Long.valueOf(v.split(" ")[0])));
+                .collect(Collectors.toMap(k -> k.split(" ")[1], v -> Long.valueOf(v.split(" ")[0]),
+                        (oldValue, newValue) -> oldValue + newValue, LinkedHashMap::new));
         assertThat(frequentWords.get("First"), Is.is(2L));
-        assertThat(frequentWords.get("Second"), Is.is(1L));
+        assertThat(frequentWords.get("Second"), Is.is(3L));
         assertThat(frequentWords.get("Third"), Is.is(1L));
+        assertThat(frequentWords.keySet(), Matchers.contains("Second", "First", "Third"));
     }
 
 }
